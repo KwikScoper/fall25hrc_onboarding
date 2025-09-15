0c47195bccb14cb7e7c6fd6dc2e79205a736e902
KwikScoper <115300617+KwikScoper@users.noreply.github.com>
2025-09-14 22:26:57 -0400

    dog walking successfully

--- visualize_robotdog.py
+++ visualize_robotdog.py
@@ -35,10 +35,10 @@ def view_floor():
     Kp = 5.0
     Kd = 0.1
 
-    gait_period = 4.0
+    gait_period = 1.0
 
-    mid_positions = [0.0, -0.3, -0.6, -0.3, 0.6, 0.3]
-    bot_positions = [0.0, 0.1, 0.2, 0.3, 0.4, 0.2]
+    mid_positions = [0.10,  0.04,  0.00, -0.06, -0.12, -0.04]
+    bot_positions = [0.00,  0.06,  0.18,  0.30,  0.18,  0.04]
 
     #joint trajectory
     freq = 1.0
@@ -51,7 +51,6 @@ def view_floor():
 
 
     viewer2 = viewer.launch_passive(model, data)
-
 
     while viewer2.is_running():
         # indexing leg joints
@@ -79,7 +78,6 @@ def view_floor():
             cycle_pos = (t / gait_period + phase) % 1.0
 
             interp_index = cycle_pos * (len(mid_positions) - 1)
-
 
             # roll joint
             qpos_des[idxs["roll"]] = roll_angle * roll_sign[leg]
@@ -110,22 +108,6 @@ def view_floor():
         t += dt
         time.sleep(dt)
         viewer2.sync()
-
-
-
-#        print(model.joint('jtopFL_roll'))
-#        print(model.joint('jtopBL_roll'))
-#        print(model.joint('jtopFR_roll'))
-#        print(model.joint('jtopBR_roll'))
-#        print(model.joint('jmidFL_pitch'))
-#        print(model.joint('jmidBL_pitch'))
-#        print(model.joint('jmidFR_pitch'))
-#        print(model.joint('jmidBR_pitch'))
-#        print(model.joint('jbotBL_pitch'))
-#        print(model.joint('jbotFL_pitch'))
-#        print(model.joint('jbotFR_pitch'))
-#        print(model.joint('jbotBR_pitch'))
-        print("______________")
 
 
 if __name__ == "__main__":
